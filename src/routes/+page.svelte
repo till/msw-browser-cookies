12ffa90d703718a8e5dc82a423609e20ae2d6ca2
till <till@php.net>
2023-02-28 14:23:19 +0100

    Chore: add a request to mockBin

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -28,6 +28,42 @@
     /** @type {boolean} */
     let loginStatusIndicator = false
 
+    /**
+     * @param {any} val
+     * @returns {string}
+     */
+    function prettyJson(val) {
+        return JSON.stringify(val, null, 2)
+    }
+
+    /**
+     * @returns {Promise<Object>}
+     */
+    async function mockBin() {
+        const response = await fetch(
+            "http://mockbin.com/request?foo=bar&foo=baz",
+            {
+                "method": "POST",
+                "headers": {
+                    "accept": "application/json",
+                    "content-type": "application/json",
+                    "cookie": "foo=bar; bar=baz",
+                    "x-pretty-print": "2"
+                },
+                "body": JSON.stringify({
+                    "foo": "bar"
+                }),
+            })
+        if (response.ok) {
+            return response.json()
+        }
+        console.error(response)
+        return {
+            error: true,
+            msg: response.text(),
+        }
+    }
+
     function generatePassword() {
         return '10823Till##'
     }
@@ -122,65 +158,98 @@ pre {
     font-family: 'Courier New', Courier, monospace;
     font-size: 10pt;
 }
+.container {
+    display: flex;
+}
+.container div {
+    flex: 1;
+    padding: 10px;
+    border: 2px solid green;
+}
 </style>
 <h1>MSW Example</h1>
 
-{#if loginStatusIndicator}
-    You are logged in!
-    {#await getLogoutLink()}
-        ... getting a link
-    {:then link}
-        [<a href="{link}">log out</a>]
-    {/await}
-{:else}
-    Not logged in! Create an account? 👇
-{/if}
-
-{#if !loginStatusIndicator && !loginFlow}
-<div>
-    <h2>Step 0/1: Let's get a flow!</h2>
-    <button on:click={createFlow}>Initialize flow</button>
-</div>
-{/if}
-
-{#if loginFlow && !accountResponse}
-<div>
-    <h2>Step 1/1: Here's the flow</h2>
-    <pre>
-        {JSON.stringify(loginFlow, null, 2)}
-    </pre>
-    <pre>
-        {JSON.stringify(csrfToken, null, 2)}
-    </pre>
-</div>
-
-<div>
-    <h2>Step 2/1: Let's create a user account</h2>
-    <form on:submit|preventDefault={submitFlow}>
-        <input type="text" placeholder="email" name="email">
-        <input type="submit" value="create account">
-    </form>
-
-    {#if errorMsg.length > 0}
-        <h3>Errors</h3>
-
-        <ul>
-        {#each errorMsg as msg}
-            <li class="error">{msg}</li>
-        {/each}
-        </ul>
+<div class="container">
+    <div>
+    {#if loginStatusIndicator}
+        You are logged in!
+        {#await getLogoutLink()}
+            ... getting a link
+        {:then link}
+            [<a href="{link}">log out</a>]
+        {/await}
+    {:else}
+        Not logged in! Create an account? 👇
     {/if}
-</div>
-{/if}
 
-{#if accountResponse}
-<div>
-    <h2>Step 2/2 (Done): You are now logged in!</h2>
-    <p>
-        <a href="/" rel="external">Reload!</a>
-    </p>
-    <pre>
-        {JSON.stringify(accountResponse, null, 2)}
-    </pre>
+    {#if !loginStatusIndicator && !loginFlow}
+        <div>
+            <h2>Step 0/1: Let's get a flow!</h2>
+            <button on:click={createFlow}>Initialize flow</button>
+        </div>
+    {/if}
+
+    {#if loginFlow && !accountResponse}
+        <div>
+            <h2>Step 1/1: Here's the flow</h2>
+            <pre>
+                {prettyJson(loginFlow)}
+            </pre>
+            <pre>
+                {prettyJson(csrfToken)}
+            </pre>
+        </div>
+
+        <div>
+            <h2>Step 2/1: Let's create a user account</h2>
+            <form on:submit|preventDefault={submitFlow}>
+                <input type="text" placeholder="email" name="email">
+                <input type="submit" value="create account">
+            </form>
+
+            {#if errorMsg.length > 0}
+                <h3>Errors</h3>
+                <ul>
+                {#each errorMsg as msg}
+                    <li class="error">{msg}</li>
+                {/each}
+                </ul>
+            {/if}
+        </div>
+    {/if}
+
+    {#if accountResponse}
+        <div>
+            <h2>Step 2/2 (Done): You are now logged in!</h2>
+            <p>
+                <a href="/" rel="external">Reload!</a>
+            </p>
+            <pre>
+                {prettyJson(accountResponse)}
+            </pre>
+        </div>
+    {/if}
+    </div>
+    <div>
+        <h2>async request</h2>
+        <div>
+        {#await mockBin()}
+            ... loading mockbin
+        {:then response}
+            <h3>headers</h3>
+            <pre>
+                {prettyJson(response.headers)}
+            </pre>
+            <h3>postData</h3>
+            <pre>
+                {prettyJson(response.postData)}
+            </pre>
+        {:catch exception}
+            An error occurred (DNS, network?):
+            <pre class="error">
+                {exception}
+            </pre>
+        {/await}
+        </div>
+    </div>
 </div>
-{/if}
